f88c1f1131a72a1d12869d13037c93d258e025d7
unknown <donallowsleywilliams@gmail.com>
2021-09-06 20:08:34 -0400

    Add fake_miner script

---
+++ Lab 1/fake_miner.py
@@ -0,0 +1,16 @@
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+def valid():
+    print(f"{bcolors.OKBLUE}Test{bcolors.ENDC}")
+
+if __name__ == '__main__':
+    valid()
