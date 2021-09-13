8c3ee12053e2fe5aab3362f130441f3001eea447
Donal Lowsley-Williams <donallowsleywilliams@gmail.com>
2021-09-12 22:10:16 -0400

    remove unnecessary bcolors

--- Lab 1/miner.py
+++ Lab 1/miner.py
@@ -1,15 +1,9 @@
 import time
 
 class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
     OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
     FAIL = '\033[91m'
     ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
 
 MESSAGE_WAIT = 2
 
