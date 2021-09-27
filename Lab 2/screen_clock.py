a37646716334099a5ec194e1378bed80b3e13987
donallw <donallowsleywilliams@gmail.com>
2021-09-27 17:10:55 -0400

    Finish Lab2ii

--- Lab 2/screen_clock.py
+++ Lab 2/screen_clock.py
@@ -1,9 +1,13 @@
 import time
 import subprocess
+import random
 import digitalio
 import board
+import busio
 from PIL import Image, ImageDraw, ImageFont
+import adafruit_apds9960.apds9960
 import adafruit_rgb_display.st7789 as st7789
+from i2c_button import I2C_Button
 
 # Configuration for CS and DC pins (these are FeatherWing defaults on M0/M4):
 cs_pin = digitalio.DigitalInOut(board.CE0)
@@ -53,7 +57,7 @@ x = 0
 # Alternatively load a TTF font.  Make sure the .ttf font file is in the
 # same directory as the python script!
 # Some other nice fonts to try: http://www.dafont.com/bitmap.php
-font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 17)
+font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 14)
 progFont = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf", 21)
 
 # Turn on the backlight
@@ -61,15 +65,38 @@ backlight = digitalio.DigitalInOut(board.D22)
 backlight.switch_to_output()
 backlight.value = True
 
+# Button setup code
+buttonA = digitalio.DigitalInOut(board.D23)
+buttonB = digitalio.DigitalInOut(board.D24)
+buttonA.switch_to_input()
+buttonB.switch_to_input()
+mainFill = "#FF2D00"
+
+i2c = busio.I2C(board.SCL, board.SDA)
+print('I2C devices found:', [hex(n) for n in i2c.scan()])
+default_addr = 0x6f
+sensor_addr = 0x39
+button = I2C_Button(i2c)
+button.clear()
+button.led_bright=0
+button.led_gran = 1
+button.led_cycle_ms = 0
+button.led_off_ms = 100
+
+oldmin = 10
+
+sensor = adafruit_apds9960.apds9960.APDS9960(i2c)
+sensor.enable_proximity = True
+
 while True:
     # Draw a black filled box to clear the image.
     draw.rectangle((0, 0, width, height), outline=0, fill=0)
-#TODO: Lab 2 part D work should be filled in here. You should be able to look in cli_clock.py and stats.py 
+    #TODO: Lab 2 part D work should be filled in here. You should be able to look in cli_clock.py and stats.py 
     currHour = int(time.strftime("%H"))
     currMinTens = int(time.strftime("%M")) // 10
     currMinOnes = int(time.strftime("%M")) % 10
     currSec = int(time.strftime("%S"))
-
+    
     hourArray = [i for i in range(12)]
     minArrayTens = [i for i in range(6)]
     minArrayOnes = [i for i in range(10)]
@@ -79,7 +106,7 @@ while True:
     y = top
     x = 0
     for hour in hourArray:
-        fill = "#FF2D00" if hour == (currHour % 12) else "#FFFFFF"
+        fill = mainFill if hour == (currHour % 12) else "#FFFFFF"
         hour = 12 if hour == 0 else hour
         toPrint = str(hour) + " "
         draw.text((x, y), toPrint, font=font, fill=fill)
@@ -88,14 +115,14 @@ while True:
     x = width / 3
     y += font.getsize("sample")[1]
     for ticker in ["AM", "PM"]:
-        fill = "#FF2D00" if ticker == currTicker else "#FFFFFF"
+        fill = mainFill if ticker == currTicker else "#FFFFFF"
         draw.text((x, y), ticker, font=font, fill=fill)
         x += width / 3 
     
     x = width / 4
     y += font.getsize("sample")[1]
     for tens in minArrayTens:
-        fill = "#FF2D00" if currMinTens == tens else "#FFFFFF"
+        fill = mainFill if currMinTens == tens else "#FFFFFF"
         toPrint = str(tens) + " "
         draw.text((x, y), toPrint, font=font, fill=fill)
         x += font.getsize(toPrint)[0]        
@@ -103,10 +130,21 @@ while True:
     x = width / 5
     y += font.getsize("sample")[1]
     for ones in minArrayOnes:
-        fill = "#FF2D00" if currMinOnes == ones else "#FFFFFF"
+        fill = mainFill if currMinOnes == ones else "#FFFFFF"
         toPrint = str(ones) + " "
         draw.text((x, y), toPrint, font=font, fill=fill)
-        x += font.getsize(toPrint)[0]        
+        x += font.getsize(toPrint)[0] 
+        if currMinOnes != oldmin:
+            print('madeit')
+            button.led_bright = 255
+            button.led_gran = 1
+            button.led_cycle_ms = 500
+            button.led_off_ms = 250 
+            oldmin = currMinOnes
+            time.sleep(1)
+        else:
+            button.led_bright = 0
+            button.led_gran = 1      
 
     x = 0
     y += font.getsize("sample")[1]
@@ -115,8 +153,24 @@ while True:
     # toPrint = '|' * currSec
     
     draw.text((x, y), toPrint, font=progFont, fill="#FFFFFF")    
-      
-    
+
+    x = 0
+    y += progFont.getsize("sample")[1]
+    print(sensor.proximity)
+    if sensor.proximity > 15:
+        draw.text((x, y), "Clear the area! Object detected.", font=font, fill=mainFill) 
+ 
+    x = 0
+    y += font.getsize("sample")[1]
+    if not buttonA.value and not buttonB.value:
+        draw.text((x, y), "Only press one button!", font=font, fill=mainFill)   
+    elif not buttonA.value:
+        # Randomize color
+        mainFill = '#' + ''.join([hex(random.randint(16, 255))[2:].upper() for i in range(3)])
+    elif not buttonB.value:
+        # Reset color to normal red
+        mainFill = '#FF2D00'
+
     # Display image.
     disp.image(image, rotation)
     time.sleep(1)
